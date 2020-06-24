07f8a27848b96654763cd4bbe1208881294cabe3
zare926 <ilove.boc926@gmail.com>
2020-06-25 00:00:43 +0900

    jsでli作成

--- js/main.js
+++ js/main.js
@@ -6,14 +6,24 @@
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+    getEl(){
+      return this.el;
+    }
   }
-  
+
   class Board {
     constructor(){
       this.panels = [];
-      for (let i = 4; i < 4; i++){
-        this.panels.push(new this.panels());
+      for (let i = 0; i < 4; i++){
+        this.panels.push(new Panel());
       }
+      this.setup();
+    }
+    setup(){
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl()); //カプセル化
+      });
     }
   }
 
