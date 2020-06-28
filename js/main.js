c829286c2eaf6426941763d059961ec93a054ffd
zare926 <ilove.boc926@gmail.com>
2020-06-28 16:54:12 +0900

    ゲームの難易度変更

--- js/main.js
+++ js/main.js
@@ -19,12 +19,12 @@
     }
 
     check(){
-      if (currentNum === parseInt(this.el.textContent, 10)) {
+      if (this.game.getCurrentNum() === parseInt(this.el.textContent, 10)) {
         this.el.classList.add('pressed');
-        currentNum++;
+        this.game.addCurrentNum();
 
-        if (currentNum === 4 ) {
-          clearTimeout(timeoutId);
+        if (this.game.getCurrentNum() === this.game.getLevel() ** 2 ) {
+          clearTimeout(this.game.getTimeoutId());
         }
       }
     }
@@ -32,9 +32,9 @@
 
   class Board {
     constructor(game){
-      this.game = game
+      this.game = game;
       this.panels = [];
-      for (let i = 0; i < 4; i++){
+      for (let i = 0; i < this.game.getLevel() ** 2; i++){
         this.panels.push(new Panel(this.game));
       }
       this.setup();
@@ -47,7 +47,10 @@
     }
 
     activate(){
-      const nums = [0, 1, 2, 3];
+      const nums = [];
+      for (let i = 0; i < this.game.getLevel() ** 2; i++){
+        nums.push(i);
+      }
       this.panels.forEach(panel => {
         const num = nums.splice(Math.floor(Math.random() * nums.length),1)[0];
         panel.activate(num);
@@ -56,7 +59,8 @@
   }
 
   class Game {
-    constructor(){
+    constructor(level){
+      this.level = level;
       this.board = new Board(this);
 
       this.currentNum = undefined;
@@ -65,7 +69,7 @@
       
       const btn = document.getElementById('btn');
       btn.addEventListener('click', () => {
-        this.startTime();
+        this.start();
       });
     }
 
@@ -84,10 +88,26 @@
       const timer = document.getElementById('timer');
       timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(2);
   
-      timeoutId = setTimeout(() => {
+      this.timeoutId = setTimeout(() => {
         this.runTimer();
       },10);
     }
+
+    addCurrentNum() {
+      this.currentNum++;
+    }
+
+    getCurrentNum(){
+      return this.currentNum;
+    }
+
+    getTimeoutId(){
+      return this.timeoutId;
+    }
+
+    getLevel() {
+      return this.level;
+    }
   }
-  new Game();
+  new Game(2);
 }
